6ca315d21f32e5b2c3be119f61089c306e4b1c38
TAVI <octavianpatilea@gmail.com>
2023-04-26 14:49:00 +0300

    lab 4 ic
    Took 39 minutes

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -275,4 +275,39 @@ public class ServiceTest {
         assertEquals(0, result);
         assertEquals(initialSize, finalSize);
     }
+
+    @Test
+    public void saveGradeUnsuccessful() {
+        String idStudent = "1";
+        String idTema = "1";
+        String feedback = "feedback";
+        int grade = 10;
+        long initialSize = service.findAllNote().spliterator().getExactSizeIfKnown();
+        int result = service.saveNota(idStudent, idTema, grade, 1, feedback);
+        long finalSize = service.findAllNote().spliterator().getExactSizeIfKnown();
+        assertEquals(-1, result);
+        assertEquals(initialSize, finalSize);
+    }
+
+    @Test
+    public void saveGradeSuccessfulBigBang() {
+        String idStudent = "1";
+        String name = "nume";
+        int group = 999;
+        service.saveStudent(idStudent, name, group);
+
+        String idTema = "1";
+        String description = "desc";
+        int deadline = 2;
+        int startline = 1;
+        service.saveTema(idTema, description, deadline, startline);
+
+        String feedback = "feedback";
+        int grade = 10;
+        long initialSize = service.findAllNote().spliterator().getExactSizeIfKnown();
+        int result = service.saveNota(idStudent, idTema, grade, 1, feedback);
+        long finalSize = service.findAllNote().spliterator().getExactSizeIfKnown();
+        assertEquals(1, result);
+        assertEquals(initialSize + 1, finalSize);
+    }
 }
